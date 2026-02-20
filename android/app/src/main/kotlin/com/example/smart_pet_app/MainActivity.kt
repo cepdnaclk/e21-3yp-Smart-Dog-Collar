33b6e1bf1a7b4c37926a246ad937e36822b58a3b
FERNANDO W.W.C.C.J. <e21137@eng.pdn.ac.lk>
2026-02-20 13:59:02 +0530

    Revert "Added working code to the main by chandur"
    This reverts commit 15a6d41c19ba1a6e696b135aaad28a87ce1ef1fd.

--- android/app/src/main/kotlin/com/example/smart_pet_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_pet_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/constants/routes.dart
+++
@@ -1,6 +0,0 @@
-class Routes {
-  static const String loading = '/';
-  static const String login = '/login';
-  static const String adminDashboard = '/admin-dashboard';
-  static const String userDashboard = '/user-dashboard';
-}
